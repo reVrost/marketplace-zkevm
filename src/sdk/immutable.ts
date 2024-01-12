5463e62beda409d8bf1484ebb7afa9e7d27c5340
Kenley Bastari <kenley.bastari@immutable.com>
2024-01-12 11:30:39 +1100

    feat: once again update passport

--- src/sdk/immutable.ts
+++ src/sdk/immutable.ts
@@ -37,11 +37,11 @@ export const passportSDK = new passport.Passport({
     environment: config.Environment.SANDBOX,
   }),
   // non production client id
-  clientId: "4jz9egnVkDgdrYQZZH8yFuvbNY1GyqVb",
+  clientId: "Ts6WetWvIEsQdSIE5swduFkGljjtyhVE",
   // default next dev hosting
-  redirectUri: "https://james-working--eloquent-muffin-915c1e.netlify.app/",
+  redirectUri: "https://james-working--eloquent-muffin-915c1e.netlify.app",
   logoutRedirectUri:
-    "https://james-working--eloquent-muffin-915c1e.netlify.app/",
+    "https://james-working--eloquent-muffin-915c1e.netlify.app",
   audience: "platform_api",
   scope: "openid offline_access email transact",
 });
